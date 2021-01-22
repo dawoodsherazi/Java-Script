4b73b3a23eecd8fcaedf9206d5a4e13388caed98
dawoodSherazi <dawoodsherazi1993@gmail.com>
2021-01-22 16:17:27 +0500

    2nd commit js not working

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
+function checkValidity() {
+    var ValidateEmail = document.getElementById("email").value;
+    var ValidatePassword = document.getElementById("password").value;
+    debugger;
 
-        function checkValidation()
-        {
-                var ValidateEmail=document.myForm.email.value;
-                var ValidatePassword=document.myForm.password.value;
-            
-                if (ValidateEmail == "dawood@gmail.com") && (ValidatePassword=="12345")
-                {
-                    document.myForm.paragraph.innerHTML="Valid Credentials";
-                    alert(valid );
-//                    document.getElementById("b1").disabled = false;
-                    return true;
-                 
-                }
-            else{
-                
-                   document.myForm.paragraph.innerHTML=" Invalid Credentials";
-//                    document.getElementById("b1").disabled = true;
-                 alert(invalid );
-                    return false;
-            }
-        }
-    
+    if (ValidateEmail == "dawood@gmail.com" && ValidatePassword == "12345") {
+
+        var mgs= "valid"
+        window.alert(mgs);
+        
+        return true;
+
+
+    } else {
+
+
+     var mgs1="invalid"
+        window.alert(mgs1);
+        return false;
+    }
+}
